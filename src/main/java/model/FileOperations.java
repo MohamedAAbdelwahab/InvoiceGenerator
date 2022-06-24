1c32cb3aaecbf95a02630c646e7f78afe9291dcf
mohamed <Mohammedseleem21@gmail.com>
2022-06-24 15:36:33 +0200

    Switching from intellj to NetBeans IDE

---
+++ src/main/java/model/FileOperations.java
@@ -0,0 +1,37 @@
+package model;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileOperations {
+
+    @Test
+    public ArrayList<InvoiceHeader> readFile() throws FileNotFoundException {
+        ArrayList<String>data=new ArrayList<>();
+        ArrayList<InvoiceHeader> headers=new ArrayList<>();
+        Scanner sc = new Scanner(new File("C:\\Users\\moham\\Desktop\\SalesInvoiceGeneratordData\\InvoiceHeader.csv"));
+        while (sc.hasNext()) {
+            data.add(sc.nextLine());
+        }
+        sc.close();
+        for (String datum : data) {
+            InvoiceHeader header=new InvoiceHeader();
+            String[] splittedData = datum.split(",");
+            header.setInvoiceNum(splittedData[0]);
+            header.setInvoiceDate(splittedData[1]);
+            header.setCustomerName(splittedData[2]);
+            headers.add(header);
+        }
+//
+        return headers;
+    }
+
+    void writeFile(ArrayList<InvoiceHeader>headers)
+    {
+
+    }
+}
---
+++ src/main/java/model/InvoiceHeader.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+
+public class InvoiceHeader {
+
+    String invoiceNum;
+    String invoiceDate;
+    String customerName;
+    ArrayList<InvoiceLine> invoiceLines = new ArrayList<>();
+
+    public String getInvoiceNum() {
+        return invoiceNum;
+    }
+
+    public void setInvoiceNum(String invoiceNum) {
+        this.invoiceNum = invoiceNum;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public ArrayList<InvoiceLine> getInvoiceLines() {
+        return invoiceLines;
+    }
+
+    public void setInvoiceLines(ArrayList<InvoiceLine> invoiceLines) {
+        this.invoiceLines = invoiceLines;
+    }
+
+}
